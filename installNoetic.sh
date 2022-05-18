e2bd2964248355c2f011ea01d6a483e82814f452
LucFabresse <luc.fabresse@gmail.com>
2022-05-18 10:23:11 +0200

    update install script

--- installNoetic.sh
+++ installNoetic.sh
@@ -5,25 +5,23 @@ sudo adduser $USER dialout
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt update
-sudo apt install ros-noetic-desktop-full
+
+sudo apt install openssh-server net-tools git vscode
+
+sudo apt install ros-noetic-desktop-full ros-noetic-catkin
 source /opt/ros/noetic/setup.bash
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-
-# additionnal packages
-# sudo apt install -y ros-noetic-urg-node
+source $HOME/.bashrc
 
 # sudo rosdep init
 # rosdep update
 
-# Make the workspace
-mkdir ~/catkin_ws
-cd catkin_ws
-mkdir -p src
+mkdir -p $HOME/catkin_ws/src
+cd $HOME/catkin_ws
 catkin_make
 echo "source $HOME/catkin_ws/devel/setup.bash" >> ~/.bashrc
 
 echo "export ROS_MASTER_URI=http://localhost:11311" >> ~/.bashrc
 
-source ~/.bashrc
+source $HOME/.bashrc
 printenv | grep ROS
